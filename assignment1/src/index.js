d3428fbb9a0dc059d503d61e79ef2c03c6997abd
Ajidokwu Sabo <realjaiboi70@gmail.com>
2024-01-21 01:34:40 +0100

    assignment

--- assignment1/src/index.js
+++ assignment1/src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <main className="container">
+      <Header />
+      <Card
+        name="Name: Ajidokwu Sabo"
+        about="ABOUT: I am a junior react and solidity developer looking on to working on multilpe projects. Currently in a 4months boothcamp in lagos"
+        stack="STACK"
+        stack1="React, solidity, and javaScript"
+        stack2=""
+        photoName="picture.jpg"
+      />
+    </main>
+  );
+}
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Welcome this is my Developer's card</h1>
+    </header>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="card">
+      <div>
+        <img className="picture" src={props.photoName} alt={props.name} />
+      </div>
+      <h3>{props.name}</h3>
+      <p>{props.about}</p>
+      <p>{props.stack}</p>
+      <p>{props.stack1}</p>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
