4c0e691e5dad8524ff871af6aca037aa7b5c504d
Ajidokwu Sabo <realjaiboi70@gmail.com>
2024-01-28 00:54:48 +0100

    commit

--- Classwork/datecounter/src/App.js
+++ Classwork/datecounter/src/App.js
@@ -3,24 +3,22 @@ export default function App() {
   const [step, setStep] = useState(0);
   const [count, setCount] = useState(0);
   // const [today, setToday] = useState(new Date().toLocaleDateString());
-  const [today, setToday] = useState(
-    new Date().toLocaleDateString("en-US", {
-      weekday: "short",
-      month: "short",
-      year: "numeric",
-      day: "numeric",
-    })
-  );
-  // const todayString = `Today is ${new Date().toLocaleDateString('en-US', { weekday: 'long', month: 'short', day: 'numeric', year: 'numeric' })}`;
-
-  // const todayString = `Today is ${new Date().toLocaleDateString('en-US', { weekday: 'long', month: 'short', year: 'numeric' })}`;
-
+  // const [today, setToday] = useState(
+  //   new Date().toLocaleDateString("en-US", {
+  //     weekday: "short",
+  //     month: "short",
+  //     year: "numeric",
+  //     day: "numeric",
+  //   })
+  // );
+  const date = new Date("Januaery 28 2024");
+  date.setDate(date.getDate() + count);
   return (
     <>
       <div>
         <button
           onClick={() => {
-            setStep(step - 1);
+            setStep((s) => s - 1);
           }}
         >
           -
@@ -28,7 +26,7 @@ export default function App() {
         step: {step}
         <button
           onClick={() => {
-            setStep(step + 1);
+            setStep((s) => s + 1);
           }}
         >
           +
@@ -38,7 +36,7 @@ export default function App() {
       <div>
         <button
           onClick={() => {
-            setCount(count - step);
+            setCount((c) => c - step);
           }}
         >
           -
@@ -47,16 +45,23 @@ export default function App() {
         <button
           onClick={() => {
             // setCount(count + 1);
-            setCount(count + step);
+            setCount((c) => c + step);
             // setToday(today + count);
           }}
         >
           +
         </button>
       </div>
-      <p>{today}</p>
-
-      {/* <p>{today ? `Today is ${today}` : `{step} days from Today is {today}`}</p> */}
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count < 0
+            ? `${Math.abs(count)} days ago was `
+            : `${count} days from now is `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
     </>
   );
 }
