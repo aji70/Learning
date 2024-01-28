3d56aa2124320f72a9efaeceba8e63ed04f6bd9e
Ajidokwu Sabo <realjaiboi70@gmail.com>
2024-01-29 00:09:06 +0100

    flashCardUpdate

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,16 +1,21 @@
 import { useState } from "react";
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: true },
-  { id: 3, description: "charger", quantity: 1, packed: true },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "charger", quantity: 1, packed: true },
+//   { id: 4, description: "laptop charger", quantity: 1, packed: true },
+// ];
 
 function App() {
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className="App">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -19,16 +24,19 @@ function App() {
 function Logo() {
   return <h1>Far away</h1>;
 }
-function Form() {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState("1");
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState(" ");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
     if (!description) return;
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    setDescription("");
-    setQuantity("1");
+    // console.log(newItem);
+    onAddItems(newItem);
   }
+  setDescription(" ");
+  setQuantity("1");
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
@@ -52,11 +60,11 @@ function Form() {
     </form>
   );
 }
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
