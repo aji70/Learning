cda3ed98a15208561dc2fc03272c26c6bc873b74
Ajidokwu Sabo <realjaiboi70@gmail.com>
2024-08-08 11:40:19 +0100

    update

--- eatandsplit/src/App.js
+++ eatandsplit/src/App.js
@@ -25,7 +25,9 @@ export default function App() {
       <div className="sidebar">
         <FriendsList />
         <FormAddFriend />
+        <Button>Add Friend</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -80,4 +82,29 @@ function FormAddFriend() {
 
 function Button({ children }) {
   return <button className="button">{children}</button>;
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>Bill Value</label>
+      <input type="text" />
+
+      <label>Your Expenses</label>
+      <input type="text" />
+
+      <label>X's Expenses</label>
+      <input type="text" disabled />
+
+      <label>Who is Paying the bill</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
+  );
 }
