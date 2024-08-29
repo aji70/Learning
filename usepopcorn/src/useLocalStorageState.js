7d737e233cf6ed672f0aacb9f9c27c95ff64cf1d
Ajidokwu Sabo <realjaiboi70@gmail.com>
2024-08-29 12:25:32 +0100

    useLocalStorage

--- usepopcorn/src/useLocalStorageState.js
+++ usepopcorn/src/useLocalStorageState.js
@@ -3,7 +3,6 @@ export function useLocalStorageState(initialState, key) {
   const [value, setValue] = useState(function () {
     const storedValue = localStorage.getItem(key);
     return storedValue ? JSON.parse(storedValue) : initialState;
-    // return storedValue;
   });
 
   useEffect(
