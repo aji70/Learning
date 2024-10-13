770e5a7a3b628cb8bb372292b7b3056f304899bc
Aji Sabo <realjaiboi70@gmail.com>
2024-10-13 20:13:19 +0100

    restarting a quiz

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -15,6 +15,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -40,17 +41,30 @@ function reducer(state, action) {
       return { ...state, index: state.index + 1, answer: null };
 
     case "finished":
-      return { ...state, status: "finished" };
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "restart":
+      return {
+        ...state,
+        questions: state.questions,
+        status: "active",
+        index: 0,
+        answer: null,
+        points: 0,
+        highscore: state.highscore,
+      };
     default:
       throw new Error("Action is unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [{ questions, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialState);
   console.log(questions);
 
   const numQuestions = questions.length;
@@ -102,6 +116,8 @@ export default function App() {
           <FinishedScreen
             points={points}
             maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
