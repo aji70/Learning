da324cb39633ee3edc0f184e6c5b2adc2d3dbb1a
Aji Sabo <realjaiboi70@gmail.com>
2024-10-30 12:26:00 +0100

    cities

--- worldwise/src/components/CityList.jsx
+++ worldwise/src/components/CityList.jsx
@@ -1,6 +1,29 @@
+import PropTypes from "prop-types"; // Imp
+import Spinner from "./Spinner";
 import styles from "./CityList.module.css";
-function CityList() {
-  return <ul className={styles.CityList}></ul>;
+import CityItem from "./CityItem";
+
+function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+  return (
+    <ul className={styles.CityList}>
+      {cities.map((city) => (
+        <CityItem key={city.id} city={city} />
+      ))}
+    </ul>
+  );
 }
+
+// // Add prop type validation
+// CityList.propTypes = {
+//   cities: PropTypes.arrayOf(
+//     PropTypes.shape({
+//       id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+//       name: PropTypes.string.isRequired,
+//       population: PropTypes.number, // Adjust as per your data
+//     })
+//   ).isRequired,
+//   isLoading: PropTypes.bool.isRequired,
+// };
 
 export default CityList;
