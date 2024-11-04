7117f4df1f77598d0b4b32eeca39f8cb83e992f3
Aji Sabo <realjaiboi70@gmail.com>
2024-11-04 12:45:54 +0100

    useNavigate

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -9,6 +9,7 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URl = "http://localhost:9000";
 function App() {
@@ -51,7 +52,7 @@ function App() {
             path="countries"
             element={<CountryList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="form" element={<p>Form</p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
