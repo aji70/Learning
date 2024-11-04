ef70ce24685535430ebb4f6ec674f09fa8f415b8
Aji Sabo <realjaiboi70@gmail.com>
2024-11-04 11:08:09 +0100

    useParams

--- worldwise/src/components/CityItem.jsx
+++ worldwise/src/components/CityItem.jsx
@@ -9,10 +9,10 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  const { cityName, emoji, date } = city;
+  const { cityName, emoji, date, id } = city;
   return (
     <li>
-      <Link className={styles.cityItem}>
+      <Link className={styles.cityItem} to={`${id}`}>
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
