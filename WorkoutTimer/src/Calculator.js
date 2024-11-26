c9840ba2045e1f637fca90d9cc657fd70867fd8e
Ajidokwu Sabo <realjaiboi70@gmail.com>
2024-11-26 16:20:44 +0100

    fin

--- WorkoutTimer/src/Calculator.js
+++ WorkoutTimer/src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useCallback, useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
@@ -7,12 +7,6 @@ function Calculator({ workouts, allowSound }) {
   const [speed, setSpeed] = useState(90);
   const [durationBreak, setDurationBreak] = useState(5);
   const [duration, setDuration] = useState(0);
-
-  // const playSound = useCallback(() => {
-  //   if (!allowSound) return;
-  //   const sound = new Audio(clickSound);
-  //   sound.play();
-  // }, [allowSound]);
 
   useEffect(
     function () {
@@ -40,7 +34,6 @@ function Calculator({ workouts, allowSound }) {
     [number]
   );
 
-  // const duration = (number * sets * speed) / 60 + (sets - 1) * durationBreak;
   const mins = Math.floor(duration);
   const seconds = (duration - mins) * 60;
   function handleInc() {
