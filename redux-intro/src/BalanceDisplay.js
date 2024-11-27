490225b77c5487b00964d477b4f0cce41f7e6478
Ajidokwu Sabo <realjaiboi70@gmail.com>
2024-11-27 23:28:39 +0100

    redux

---
+++ redux-intro/src/BalanceDisplay.js
@@ -0,0 +1,12 @@
+function formatCurrency(value) {
+  return new Intl.NumberFormat("en", {
+    style: "currency",
+    currency: "USD",
+  }).format(value);
+}
+
+function BalanceDisplay() {
+  return <div className="balance">{formatCurrency(123456789999993)}</div>;
+}
+
+export default BalanceDisplay;
